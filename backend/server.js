e8079e1883f5470459790ea3632a5c7d54d622bd
mboulmane <mohamed.boulmane@insa-rouen.fr>
2024-11-29 20:53:49 +0100

    reda

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,23 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// add data to req.body (for POST requests)
+app.use(express.urlencoded({ extended: true }));
+
+// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'));
+//session pour tout le monde ou bien juste admin?
 
-app.use(express.static(path.join(__dirname, '../frontend/public')));
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
+// serve static files
+app.use(express.static('../frontend/public'));
+
+const search = require('./routers/search');
+app.use('/search',search);
+
+const router = require('./routers/router');
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
