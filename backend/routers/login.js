d6965500ab5fc882cb940a08f0c040caae3b73d4
mboulmane <mohamed.boulmane@insa-rouen.fr>
2024-11-30 00:53:30 +0100

    update

--- backend/routers/login.js
+++ backend/routers/login.js
@@ -4,6 +4,12 @@ const router = express.Router();
 // add data to req.body (for POST requests)
 router.use(express.urlencoded({ extended: true }));
 
+const crypto = require('crypto');
+
+
+// Exemple d'utilisation :
+console.log(stringHashCode("exemple")); 
+
 
 router.use('/login', function (req, res) {
 	res.render('login.ejs', {logged: req.session.loggedin});
@@ -13,5 +19,15 @@ router.use('/logout', function (req, res) {
 	req.session.destroy();
 	res.redirect('/login');
 });
+
+
+//fonction pour hasher le mdp
+function stringHashCode(str) {
+    return crypto.createHash('sha256')  // Utiliser SHA-256 ou 'sha1', 'md5', etc.
+                 .update(str)
+                 .digest('hex');  // retourne le hash en hexadécimal
+}
+
+console.log(stringHashCode('admin'));
 
 module.exports = router;
