4da9fc661d268392532f4514a75f14da6a4c2a51
mboulmane <mohamed.boulmane@insa-rouen.fr>
2024-12-01 22:01:53 +0100

    fin affichage des recettes

--- backend/routers/recipes.js
+++ backend/routers/recipes.js
@@ -11,14 +11,44 @@ const db  = new sqlite3.Database('./db/reciperoulette.sqlite', (err) => {
 	console.log('Connected to the database!');
   });
 
+
+router.get('/', function (req, res, next) {
+
+  if (req.query['recipe']){
+    
+    let recipeId = req.query['recipe'];
+    //console.log(recipeId);
+
+    db.get(" SELECT * FROM RECIPE where recipeId=" +recipeId+ ";", (err, row) => {
+      if(err){
+          next(err);
+      } else {
+
+        db.all(" SELECT * FROM RECIPE_INGREDIENT where recipeId=" +recipeId+ ";", (err, rows) => {
+            if(err){
+                next(err);
+            }else{
+              //console.log(row);
+              //console.log(rows);
+              res.render('recipes.ejs', {specificRecipe:true, recipe:row, ingredients:rows, logged: req.session.loggedin});
+            }
+        });
+          
+      }
+    });
+  }else{
+    next();
+  }
+});
+
 router.use('/', function (req, res) {
 
     db.all(" SELECT * FROM RECIPE where status='VALID';", (err, rows) => {
         if(err){
             next(err);
         } else {
-            console.log(rows);
-            res.render('recipes.ejs', {recipes: rows, logged: req.session.loggedin});
+            //console.log(rows);
+            res.render('recipes.ejs', {specificRecipe:false, recipes: rows, logged: req.session.loggedin, ingredients:[]});
         }
     });
 
