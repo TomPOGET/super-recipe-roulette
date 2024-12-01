2abc866fd54777f62e0b36f31e2f2ecbc1e140c3
Yassine Zahou W <yassine.zahou@insa-rouen.fr>
2024-12-01 23:11:46 +0100

    fix

--- backend/routers/admin.js
+++ backend/routers/admin.js
@@ -11,18 +11,9 @@ function isAuthenticated(req, res, next) {
   }
 }
 
-<<<<<<< HEAD
 // Appliquer le middleware uniquement à la route /admin
 router.get('/admin', isAuthenticated, (req, res) => {
   res.render('admin.ejs'); // Rendre admin.ejs avec le nom d'utilisateur
-=======
-// Appliquer le middleware d'authentification à toutes les routes de ce routeur
-router.use(isAuthenticated);
-
-// Route pour afficher le tableau de bord administrateur
-router.get('/', (req, res) => {
-  res.render('admin'); // Ceci va rendre le fichier admin.ejs
->>>>>>> 4da9fc661d268392532f4514a75f14da6a4c2a51
 });
 
 module.exports = router;
