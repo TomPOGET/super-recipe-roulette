3e26cf17b88f3e1c2c2df1851c75451e75c3c136
mboulmane <mohamed.boulmane@insa-rouen.fr>
2024-12-02 00:09:30 +0100

    fix admin.js

--- backend/routers/admin.js
+++ backend/routers/admin.js
@@ -1,4 +1,3 @@
-// routers/admin.js
 const express = require('express');
 const router = express.Router();
 
@@ -11,9 +10,12 @@ function isAuthenticated(req, res, next) {
   }
 }
 
-// Appliquer le middleware uniquement à la route /admin
-router.get('/admin', isAuthenticated, (req, res) => {
-  res.render('admin.ejs'); // Rendre admin.ejs avec le nom d'utilisateur
+// Appliquer le middleware d'authentification à toutes les routes de ce routeur
+router.use(isAuthenticated);
+
+// Route pour afficher le tableau de bord administrateur
+router.get('/', (req, res) => {
+  res.render('admin'); // Ceci va rendre le fichier admin.ejs
 });
 
 module.exports = router;
