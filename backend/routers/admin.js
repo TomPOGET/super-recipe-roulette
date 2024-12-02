0f38f34e2ced85c64f023b58c6d3276b50dad612
mboulmane <mohamed.boulmane@insa-rouen.fr>
2024-12-02 01:15:55 +0100

    maj dashboard

--- backend/routers/admin.js
+++ backend/routers/admin.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const sqlite3 = require('sqlite3').verbose();
+
+// connecting an existing database (handling errors)
+const db  = new sqlite3.Database('./db/reciperoulette.sqlite', (err) => {
+	if (err) {
+	  console.error(err.message);
+	}
+	console.log('Connected to the database!');
+  });
+
 // Middleware pour vérifier si l'utilisateur est connecté
 function isAuthenticated(req, res, next) {
   if (req.session && req.session.loggedin) {
@@ -14,8 +24,41 @@ function isAuthenticated(req, res, next) {
 router.use(isAuthenticated);
 
 // Route pour afficher le tableau de bord administrateur
-router.get('/', (req, res) => {
-  res.render('admin'); // Ceci va rendre le fichier admin.ejs
+
+router.get('/', (req, res, next) => {
+  // Requêtes pour récupérer les recettes valides et invalides
+  const validRecipesQuery = "SELECT * FROM RECIPE WHERE status='VALID'";
+  const invalidRecipesQuery = "SELECT * FROM RECIPE WHERE status='INVALID'";
+
+  // Utiliser db.serialize pour garantir l'ordre d'exécution
+  db.serialize(() => {
+    const results = {};
+
+    // Récupérer les recettes valides
+    db.all(validRecipesQuery, (err, validRecipes) => {
+      if (err) {
+        return next(err); // Passer l'erreur au middleware suivant
+      }
+      results.validRecipes = validRecipes;
+
+      // Récupérer les recettes invalides
+      db.all(invalidRecipesQuery, (err, invalidRecipes) => {
+        if (err) {
+          return next(err); // Passer l'erreur au middleware suivant
+        }
+        results.invalidRecipes = invalidRecipes;
+
+        // Rendu de la page avec les deux listes
+        res.render('admin', {
+          validRecipes: results.validRecipes,
+          invalidRecipes: results.invalidRecipes,
+        });
+      });
+    });
+  });
 });
+
+  
+
 
 module.exports = router;
